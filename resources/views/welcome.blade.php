cd5c9eceedd538741940dab95d2808287acbb6e9
MadsVerberne <91489@roc-teraa.nl>
2025-05-28 12:35:54 +0200

    Populaire huizen sectie
    Populaire huizen sectie klaar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,11 @@
     <title>Beneluxe</title>
     <link rel="icon" type="image/x-icon" href="img/Favicon.png">
 
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -86,7 +91,63 @@
     </section>
 
     <main>
-
+        <div class="populairehuizen">
+            <h2>Populaire huizen</h2>
+            <div class="populairehuizenrow">
+                <div class="populairehuizencol">
+                    <img src="/img/Populairehuizen1.jpg" alt="Populairehuizen1">
+                    <h3>Vakantiehuis aan het strand</h3>
+                    <p>Egmond aan zee</p>
+                    <div class="ratingstars">
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-half"></i>
+                    </div>
+                    <h4>€90 · <span>2 nachten</span></h4>
+                </div>
+                <div class="populairehuizencol">
+                    <img src="/img/Populairehuizen2.png" alt="Populairehuizen2">
+                    <h3>Chalet in de bossen</h3>
+                    <p>Durbuy, België</p>
+                    <div class="ratingstars">
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-half"></i>
+                    </div>
+                    <h4>€120 · <span>3 nachten</span></h4>
+                </div>
+                <div class="populairehuizencol">
+                    <img src="/img/Populairehuizen3.png" alt="Populairehuizen3">
+                    <h3>Villa Luxemburg</h3>
+                    <p>Schieren, Luxemburg</p>
+                    <div class="ratingstars">
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                    </div>
+                    <h4>€410 · <span>7 nachten</span></h4>
+                </div>
+                <div class="populairehuizencol">
+                    <img src="/img/Populairehuizen4.jpg" alt="Populairehuizen4">
+                    <h3>Boshuisje Limburg</h3>
+                    <p>Beek, Limburg</p>
+                    <div class="ratingstars">
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star"></i>
+                    </div>
+                    <h4>€220 · <span>5 nachten</span></h4>
+                </div>
+            </div>
+        </div>
     </main>
 </body>
 
