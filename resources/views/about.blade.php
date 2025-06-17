c4501b87457e8012e6ddcdb0f328c7378529c9ed
MadsVerberne <91489@roc-teraa.nl>
2025-06-17 10:14:01 +0200

    Over ons pagina af

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,8 +8,16 @@
         </section>
         <div class="about">
             <img src="/img/Populairehuizen1.jpg" alt="">
-            <h2>Over ons</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus quaerat quas sit nulla doloribus eaque placeat. Doloribus illo nemo corrupti aliquam voluptas recusandae esse doloremque voluptate, corporis voluptatem. Quo, sunt.</p>
+            <div class="aboutinfo">
+                <h2>Meer over Beneluxe</h2>
+                <p><span>Welkom bij Beneluxe</span> — dé plek waar je de mooiste vakantiehuizen en unieke verblijven in Nederland, België en Luxemburg ontdekt.
+                Of je nu op zoek bent naar een knusse B&B in de Ardennen, een stijlvol appartement in hartje Antwerpen, of een luxe villa aan de Nederlandse kust, bij ons vind je altijd een accommodatie die perfect bij jouw wensen past.
+                Ons platform is ontstaan vanuit een passie voor reizen en gastvrijheid. Wij willen het voor zowel reizigers als verhuurders eenvoudig en transparant maken:<br><br>
+                - Voor huurders bieden we een helder en gebruiksvriendelijk boekingssysteem, met eerlijke prijzen en échte reviews.<br><br>
+                - Voor verhuurders bieden we een krachtig platform waarmee ze hun woning onder de aandacht kunnen brengen bij duizenden potentiële gasten.<br><br>
+                Met persoonlijke service, een groeiend aanbod en oog voor kwaliteit streven we er elke dag naar om jouw verblijfservaring nog beter te maken.<br>
+                Benieuwd naar ons aanbod? <span>Ontdek het zelf — en laat je inspireren!</span></p>
+            </div>
 
         </div>
     @endsection
