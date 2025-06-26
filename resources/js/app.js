403edfeeb85b9be0f895c1548509d408075d1d36
MadsVerberne <91489@roc-teraa.nl>
2025-06-26 10:30:19 +0200

    Website responsive gemaakt

--- resources/js/app.js
+++ resources/js/app.js
@@ -220,3 +220,13 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+
+/* Telefoon menu */
+document.addEventListener("DOMContentLoaded", function () {
+  const navToggle = document.querySelector(".nav-toggle");
+  const nav = document.querySelector(".nav");
+
+  navToggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+  });
+});
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,30 +1,31 @@
 <header class="header">
-    <div class="logo">
-        <a href="/">
-            <img src="/img/Favicon.png" alt="Beneluxe Logo">
-        </a>
-    </div>
+  <div class="logo">
+    <a href="/">
+      <img src="/img/Favicon.png" alt="Beneluxe Logo" />
+    </a>
+  </div>
 
-    <nav class="nav">
-        <ul>
-            <li><a href="/accommodaties">Accommodaties</a></li>
-            <li><a href="/about">Over ons</a></li>
-            <li><a href="/contact">Contact</a></li>
-        </ul>
-    </nav>
+  <button class="nav-toggle" aria-label="Toggle navigation">
+    &#9776; <!-- hamburger icoon -->
+  </button>
 
-    <div class="header-button">
-        @auth
-            <a href="{{ route('dashboard') }}">
-                <i class="bi bi-person-circle"></i>
-            </a>
-        @else
-            <a href="{{ route('login') }}">
-                {{-- <button id="openlogin">Inloggen</button> --}}
-                <button id="">Inloggen</button>
-            </a>
-        @endauth
-    </div>
+  <nav class="nav">
+    <ul>
+      <li><a href="/accommodaties">Accommodaties</a></li>
+      <li><a href="/about">Over ons</a></li>
+      <li><a href="/contact">Contact</a></li>
+    </ul>
+  </nav>
 
-
+  <div class="header-button">
+    @auth
+    <a href="{{ route('dashboard') }}">
+      <i class="bi bi-person-circle"></i>
+    </a>
+    @else
+    <a href="{{ route('login') }}">
+      <button id="">Inloggen</button>
+    </a>
+    @endauth
+  </div>
 </header>
